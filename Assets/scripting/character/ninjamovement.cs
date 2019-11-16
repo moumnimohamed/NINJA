8816a987b4fcaa7f3db676ed8b5ad6a38ff16d8a
´moumnimohamed´ <moumnimohamed93@outlook.com>
2019-11-16 19:17:06 +0100

    adding enmy ai movement

---
+++ Assets/scripting/character/ninjamovement.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ninjamovement : MonoBehaviour
+{
+
+public Transform attackPos;
+    public LayerMask whatIsenmies;
+    public float attackRangeX;
+    public float attackRangeY;
+
+public bool amAttaking =false;
+
+
+    public float moveSped =5f;
+    public Animator animator ;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    // Start is callded before the first frame update
+    void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();
+        animator=GetComponent<Animator>();
+
+        
+    }
+
+    void Flip (float value){
+       
+     Vector3 scaler =transform.localScale;
+     scaler.x=value;
+          transform.localScale=scaler;
+ }
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x=Input.GetAxisRaw("Horizontal");
+         movement.y=Input.GetAxisRaw("Vertical");
+
+       
+
+         
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       rb.MovePosition(rb.position + movement * moveSped *Time.fixedDeltaTime);
+   
+     animator.SetFloat("horizontal",movement.x);
+         animator.SetFloat("speed",movement.sqrMagnitude);
+         if(movement.x >0){
+         Flip (0.5f);
+
+         }else if(movement.x <0)
+         {
+         Flip (-0.5f);
+             
+         }
+
+
+         
+    /*attack animation */
+
+    if (Input.GetKeyDown(KeyCode.Space)  &&  amAttaking==false)
+    {
+         amAttaking=true;
+         //play random  animation
+         Random rnd = new Random();
+                  int animRang   = Random.Range(1, 3);
+                  if (animRang==1)
+                  {
+         animator.SetBool("attack",true);
+                      
+                  } else  if (animRang==2)
+                  {
+         animator.SetBool("attack2",true);
+                      
+                  }
+
+          Collider2D [] enemiestoDammage=Physics2D.OverlapBoxAll(attackPos.position,new Vector2(attackRangeX,attackRangeY),0,whatIsenmies);
+ for (int i = 0; i < enemiestoDammage.Length; i++)
+ {
+    enemiestoDammage[i].GetComponent<foxEnmy>().takeDammage(1);
+    enemiestoDammage[i].GetComponent<Collider2D>().enabled = false;
+    /*push enmy */
+    Rigidbody2D rbeenmy=enemiestoDammage[i].GetComponent<Rigidbody2D>();
+   rbeenmy.isKinematic=true;
+ rbeenmy.AddForce(transform.right *moveSped*1000);
+ }
+   //rb.AddForce(transform.right *runspeedvalue*120);
+        
+    }
+    }
+
+
+ 
+
+    public void stopAttack () {
+         animator.SetBool("attack",false);
+         amAttaking=false;
+    }
+
+     public void stopAttack2 () {
+         animator.SetBool("attack2",false);
+          amAttaking=false;
+    }
+
+
+     void OnDrawGizmosSelected()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color =Color.red;
+        Gizmos.DrawWireCube(attackPos.position, new Vector3(attackRangeX,attackRangeY,1));
+    }
+
+ }
---
+++ Assets/scripting/foxEnmy.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foxEnmy : MonoBehaviour
+{
+ 
+    
+bool enmyDead=false;
+    public int health;
+     public  int healthValue;
+       public Animator animator ;
+
+
+       public float speed=0;
+       public float retreatDistance=0;
+       public float stopingDistance=0;
+
+public Transform player;
+     
+    
+
+
+
+  private Rigidbody2D rb;
+
+    void Start()
+    {
+        
+        rb=GetComponent<Rigidbody2D>();
+        animator=GetComponent<Animator>();
+        healthValue=health;
+        player =GameObject.FindGameObjectWithTag("ninja").transform;
+        
+    }
+
+    void FixedUpdate()
+ {
+  
+//folow ninja to kill hime
+ if (Vector2.Distance(transform.position,player.position) > stopingDistance)
+ {
+     transform.position=Vector2.MoveTowards(transform.position,player.position,speed * Time.deltaTime);
+     AnimState(1);
+
+ }
+ //run far  from ninja if i attacked him
+ else if (Vector2.Distance(transform.position,player.position) < retreatDistance   )
+ {
+     transform.position=Vector2.MoveTowards(transform.position,player.position,-speed * Time.deltaTime);
+     AnimState(1);
+     
+ }
+ // stop enmy from moving 
+ //normally must not stoping
+  else if (Vector2.Distance(transform.position,player.position) < stopingDistance &&  Vector2.Distance(transform.position,player.position) > retreatDistance  )
+ {
+     transform.position= this.transform.position;
+       AnimState(0);
+     
+ }
+
+ }
+
+    // Update is called once per frame
+    void Update()
+    {    
+ 
+         
+
+       /*  if ( enmyDead==false)
+        {
+           
+                AnimState(1);
+        }
+        else if ( enmyDead==false)
+        {
+         
+                AnimState(0);
+        }*/
+    }
+ 
+
+    public void takeDammage (int dmg){
+         healthValue-=dmg;
+         if (healthValue<=0)
+         {
+             enmyDead=true;
+              
+            AnimState(3);
+            
+         }
+    }
+     
+       void OnCollisionEnter2D(Collision2D col)
+    {
+        if (enmyDead==false )
+        {
+        if(col.gameObject.name == "ninja")
+        {
+          col.gameObject.GetComponent<CharacterController2D>().deadNinja();
+        }
+    }      
+        }
+
+        void OnTriggerEnter2D(Collider2D col)
+    {
+         
+        if (col.tag == "ninja"){
+             
+             takeDammage (1);
+            //col.GetComponent<Rigidbody2D>().velocity=Vector2.up*20;
+             
+
+        }
+    }
+
+    void AnimState (int value){
+     animator.SetInteger("anistate",value);
+}
+
+ 
+ void Flip (){
+//      facengleft=  !facengleft;
+     Vector3 scaler =transform.localScale;
+     scaler.x*=-1;
+     transform.localScale=scaler;
+    
+ }
+}
